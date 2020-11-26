50031764419ee185b1886e0854689a1cb7cdc060
YiFei Zhu <zhuyifei1999@gmail.com>
2020-11-26 13:03:15 -0600

    Optimize for size, binpkg for build

---
+++ vm/build-inner.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+
+set -ex
+
+LINUX_VER=5.9.9
+PY_VER=3.9
+
+emerge-webrsync
+
+if [[ -r "${REPO}/vm/ishoal_ink_portage_bin.id_rsa" ]]; then
+  mkdir -p /root/.ssh
+  cp "${REPO}/vm/ishoal_ink_portage_bin.id_rsa" /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+
+  emerge -vn net-misc/openssh net-misc/rsync
+
+  ssh -o StrictHostKeyChecking=no -v ubuntu@ishoal.ink true
+
+  export FEATURES='buildpkg getbinpkg' PORTAGE_BINHOST='ssh://ubuntu@ishoal.ink/srv/binpkgs'
+fi
+
+emerge -vnk app-portage/repoman
+
+mkdir -p /var/db/repos/localrepo/{metadata,profiles}
+chown -R portage:portage /var/db/repos/localrepo
+echo localrepo > /var/db/repos/localrepo/profiles/repo_name
+
+cat > /var/db/repos/localrepo/metadata/layout.conf << 'EOF'
+masters = gentoo
+auto-sync = false
+EOF
+
+mkdir -p /etc/portage/repos.conf/
+cat > /etc/portage/repos.conf/localrepo.conf << 'EOF'
+[localrepo]
+location = /var/db/repos/localrepo
+EOF
+
+mkdir -p /var/db/repos/localrepo/sys-apps/bpftool/
+cp "${REPO}/vm/bpftool.ebuild" "/var/db/repos/localrepo/sys-apps/bpftool/bpftool-${LINUX_VER}.ebuild"
+chown -R portage:portage /var/db/repos/localrepo
+
+pushd /var/db/repos/localrepo/sys-apps/bpftool/
+repoman manifest
+popd
+
+# https://bugs.gentoo.org/756034
+cat > /etc/portage/package.mask << 'EOF'
+=dev-util/cmake-3.19.0
+EOF
+
+export LLVM_TARGETS=BPF ACCEPT_KEYWORDS='~amd64'
+emerge -vok sys-devel/llvm
+MAKEOPTS="-j$(( $(nproc) < 4 ? $(nproc) : 4 ))" emerge -vnk sys-devel/llvm sys-devel/clang
+unset LLVM_TARGETS ACCEPT_KEYWORDS
+
+emerge -vok gentoo-sources
+
+source /etc/profile
+
+wget "https://cdn.kernel.org/pub/linux/kernel/v${LINUX_VER%.*.*}.x/linux-${LINUX_VER}.tar.xz"
+tar xf "linux-${LINUX_VER}.tar.xz"
+mv "linux-${LINUX_VER}" kernel
+
+pushd kernel
+./scripts/kconfig/merge_config.sh ./arch/x86/configs/x86_64_defconfig "${REPO}/vm/kconfig"
+popd
+
+make -C kernel -j"$(nproc)"
+
+emerge -vnk "dev-lang/python:${PY_VER}" dev-util/dialog dev-libs/userspace-rcu
+ACCEPT_KEYWORDS='~amd64' emerge -vnk dev-libs/libbpf sys-apps/bpftool
+
+"python${PY_VER}" -m ensurepip
+
+rm "${REPO}/src/"*.d || true
+make -B -C "${REPO}/src/" PYTHON="python${PY_VER}" CLANGFLAGS='-D__x86_64__' CFLAGS='-Os -pipe -g -Wall'
+
+emerge -vnk sys-boot/gnu-efi
+make -B -C "${REPO}/vm/efi_fb_res"
+
+if [[ -r /root/.ssh/id_rsa ]]; then
+  rsync -avz --delete /var/cache/binpkgs ubuntu@ishoal.ink:/srv/
+fi
+
+unset FEATURES PORTAGE_BINHOST
+
+export USE='-* make-symlinks unicode ssl ncurses readline' CFLAGS='-Os -pipe'
+emerge --root rootfs -v sys-apps/baselayout
+emerge --root rootfs -v sys-apps/busybox
+emerge --root rootfs -v "dev-lang/python:${PY_VER}" dev-util/dialog dev-libs/userspace-rcu
+emerge --root rootfs -v sys-process/htop sys-process/lsof dev-util/strace
+ACCEPT_KEYWORDS='~amd64' emerge --root rootfs -v dev-libs/libbpf sys-apps/bpftool
+unset USE CFLAGS
+
+make -C kernel -j"$(nproc)" modules_install INSTALL_MOD_PATH="$(realpath rootfs)"
+
+GCC_PATH="$(gcc -print-search-dirs | grep install | cut -d\  -f2)"
+mkdir -p rootfs/"${GCC_PATH}"
+cp -a "${GCC_PATH}"/libgcc_s.so* rootfs/"${GCC_PATH}"
+echo "${GCC_PATH}" > rootfs/etc/ld.so.conf
+ldconfig -C rootfs/etc/ld.so.cache -f rootfs/etc/ld.so.conf
+
+find rootfs/usr/share/i18n/locales/ -mindepth 1 -maxdepth 1 ! -name 'en_US' ! -name 'en_GB' ! -name 'C' ! -name 'i18n*' ! -name 'iso*' ! -name 'translit*' -delete
+find rootfs/usr/share/i18n/charmaps/ -mindepth 1 -maxdepth 1 -name '*.gz' ! -name 'UTF*' ! -name 'LATIN*' -delete
+find rootfs/usr/lib64/gconv/ -mindepth 1 -maxdepth 1 -name '*.so' ! -name 'UTF*' ! -name 'LATIN*' ! -name 'UNICODE*' -delete
+find rootfs/usr/share/locale/ -mindepth 1 -maxdepth 1 -type d ! -name 'en_US' ! -name 'en_GB' ! -name 'C' -exec rm -r {} \;
+find rootfs/usr/share/terminfo/ -mindepth 2 -maxdepth 2 ! -name 'ansi*' ! -name 'linux*' ! -name 'vt*' ! -name 'xterm*' ! -name 'screen*' ! -name 'gnome*' -delete
+find rootfs/usr/share/terminfo/ -empty -type d -delete
+
+rm -r rootfs/usr/share/doc/
+rm -r rootfs/usr/share/man/
+rm -r rootfs/usr/share/info/
+rm -r rootfs/usr/include/
+rm -r rootfs/usr/share/misc/magic*
+rm -r rootfs/usr/lib/python*/test
+rm -r rootfs/usr/lib/python*/unittest
+find rootfs/usr/lib/python*/ -name '__pycache__' -prune -exec rm -r {} \;
+
+rm -r rootfs/var/db/pkg/
+rm -r rootfs/var/cache/edb/
+rm -r rootfs/etc/portage/
+
+rm -r rootfs/lib/gentoo/ || true
+rm -r rootfs/var/lib/gentoo/ || true
+rm -r rootfs/var/lib/portage/ || true
+
+rm -r rootfs/usr/share/gdb/ || true
+rm -r rootfs/usr/share/baselayout/ || true
+
+mkdir -p rootfs/{boot,dev,proc,root,run,sys}
+
+mknod -m 622 rootfs/dev/console c 5 1
+mknod -m 666 rootfs/dev/null c 1 3
+mknod -m 666 rootfs/dev/zero c 1 5
+mknod -m 666 rootfs/dev/ptmx c 5 2
+mknod -m 666 rootfs/dev/tty c 5 0
+mknod -m 444 rootfs/dev/random c 1 8
+mknod -m 444 rootfs/dev/urandom c 1 9
+
+ln -s /proc/self/fd rootfs/dev/fd
+ln -s /proc/self/fd/0 rootfs/dev/stdin
+ln -s /proc/self/fd/1 rootfs/dev/stdout
+ln -s /proc/self/fd/2 rootfs/dev/stderr
+mkdir -m 0755 rootfs/dev/shm
+mkdir rootfs/dev/pts
+
+mkdir -p rootfs/var/log
+ln -s /proc/self/mounts rootfs/etc/mtab
+touch rootfs/etc/fstab
+
+echo ishoal > rootfs/etc/hostname
+echo 'nameserver 8.8.8.8' > rootfs/etc/resolv.conf
+
+mkdir -p rootfs/etc/init.d/
+
+cat > rootfs/etc/init.d/rcS << 'EOF'
+#! /bin/sh
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+mount -n -t proc -o nosuid,noexec,nodev proc /proc
+mount -n -t tmpfs -o nosuid,nodev tmpfs /run
+mount -n -t sysfs -o nosuid,noexec,nodev sys /sys
+mount -n -t tmpfs -o mode=1777,nosuid,nodev tmpfs /tmp
+
+mount -n -t debugfs debugfs /sys/kernel/debug
+
+mkdir -p /dev/pts
+mount -n -t devpts -o gid=tty,mode=620,noexec,nosuid devpts /dev/pts
+mkdir -p /dev/shm
+mount -n -t tmpfs -o mode=1777,nosuid,nodev tmpfs /dev/shm
+
+mount -t bpf bpffs /sys/fs/bpf
+
+mdev -s
+mdev -d
+
+mount -o remount,rw /
+
+hostname ishoal
+
+ip link set dev lo up
+ip link set dev eth0 up
+udhcpc -i eth0 -p /run/udhcpc -s /usr/share/udhcpc/default.script -q -n -f
+
+ping -w 5 -c 1 8.8.8.8
+
+dmesg -n 1
+EOF
+chmod a+x rootfs/etc/init.d/rcS
+
+cat > rootfs/root/ishoal-wrapper << 'EOF'
+#! /bin/sh
+while true; do
+  echo 'Booting iShoal ...'
+  /root/ishoal eth0
+  EXITCODE=$?
+
+  if [ $EXITCODE -eq 2 ]; then
+    sync
+    clear
+    poweroff
+    echo 'Waiting for system shutdown.'
+    sleep 20
+  elif [ $EXITCODE -eq 3 ]; then
+    sync
+    clear
+    reboot
+    echo 'Waiting for system reboot.'
+    sleep 20
+  elif [ $EXITCODE -ne 0 ]; then
+    echo 'IShoal failed, entering shell. Please type 'exit' to exit the shell.'
+    /bin/sh
+  fi
+done
+EOF
+chmod a+x rootfs/root/ishoal-wrapper
+
+cat > rootfs/etc/inittab << 'EOF'
+::sysinit:/etc/init.d/rcS
+tty1::respawn:/root/ishoal-wrapper
+tty2::respawn:-/bin/sh
+::restart:/sbin/init
+::ctrlaltdel:/sbin/reboot
+::shutdown:/bin/umount -a -r
+EOF
+
+cp "${REPO}/src/ishoal" rootfs/root/ishoal
+chmod a+x rootfs/root/ishoal
+
+mkdir -p rootfs/boot/EFI/Boot/
+cp kernel/arch/x86/boot/bzImage rootfs/boot/linux.efi
+cp "${REPO}/vm/efi_fb_res/efi_fb_res.efi" rootfs/boot/EFI/Boot/bootx64.efi
